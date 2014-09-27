c34cb6cc3cda33bd83d4dd651ad946411e3e49da
ramasree <ramasree@users.noreply.github.com>
2014-09-27 22:03:03 +0530

    Oring with mouse click

--- oring_inspect_1.py
+++ oring_inspect_1.py
@@ -12,23 +12,27 @@ from matplotlib.figure import Figure
 from tkintertable.Tables import TableCanvas
 from tkintertable.TableModels import TableModel
 import time
-import sys
 plt.ion()
 #Capturing video frame
-cap = cv2.VideoCapture()
-cap.open(0)
+
+if not 'cap' in locals():
+	cap = cv2.VideoCapture()
+if not cap.isOpened():
+	cap.open(0)
 #~ gray_1 = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2GRAY)
 ret, frame = cap.read()
-if ret == None:
+cv2.imwrite("Image.jpg",frame)
+if ret == 0:
 	cap.release()
-	cap.open(0)
+	cap.open(1)
 	ret, frame = cap.read()
 	print 'cam failed, second attempt'
 
-#Creating window with title and geometry
+#Creating window with title and= geometry
 master = Tk()
 master.title("Oring GUI")
 master.geometry("1000x1000") #how to set full screen
+
 
 #Creating figure for showing video frame
 f1 = Figure(figsize=(4, 4), dpi=100)
@@ -86,52 +90,90 @@ def showGraph(arg):
 	canvas2.show()
 	toc = time.time()
 	print "showGraphTime", (toc -  tic)
-#Function to display video frame,mean of frame variations,display in a table
-def showFrame(hIm):
-	tic = time.time()
-	global axis1
-	#~ axis1.imshow(cap.read()[1])
-	ret, frame = cap.read()
-	if ret == None:
-		print "cap.read() failed"
-		return
-	#~ 
-	gray_scale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-	hIm.set_array(gray_scale)
-	canvas1.show()
-	#~ gray_2 = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2GRAY)
-	#~ frm_diff = cv2.absdiff(gray_1, gray_2)
-	#~ mean = np.mean(frm_diff)
-	#~ values.append(mean)
-	#~ data = {'1': {'Mean Value': '{0:.3f}'.format(mean)}}
-	#~ model = table.model
-	#~ model.importDict(data)
-	#~ table.redrawTable()
-	#~ values.remove(values[0])
-	#gray_2 = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2GRAY)
-	#~ gray_1 = gray_2
 
-	#~ if cv2.waitKey(1) & 0xFF == ord('q'):
-		#~ exit()
-	toc = time.time()
-	data = {'1': {'Time': '{0:.3f}'.format(toc - tic),'Gray_value': '{}'.format(gray_scale.min())}}
-	model = table.model
-	model.importDict(data)
-	table.redrawTable()
-	#~ print "showFrameTime", (toc - tic)
+	#~ print '\nClicked at: x=',event.x,'  y=',event.y
+	#~ ret,frame = cap.read()
+	#~ if ret == None:
+		#~ print "cap.read() failed"
+		#~ return
+	#~ hIm.set_array(frame)
+	#~ canvas1.show()
+	#~ canvas1.mpl_connect('button_press_event1', click1)
+	
+#~ def click1(event1):
+	#~ print '\nClicked at: x=',event1.x,'  y=',event1.y
+	#~ ret,frame = cap.read()
+	#~ if ret == None:
+		#~ print "cap.read() failed"
+		#~ return
+	#~ gray_scale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	#~ hIm.set_array(gray_scale)
+	#~ canvas1.show()
+	#~ canvas1.mpl_connect('button_press_event', click)
+	
+				
+#Function to display video frame,mean of frame variations,display in a table
+class ShowFrame():
+	def __init__(self):
+		self.kShowType = 0
+		self.captureDevice = None
+		self.axes = None
+		self.hIm = None
+		
+		
+	def showFrame(self):
+		tic = time.time()
+		ret, self.frame = self.captureDevice.read()
+		if ret == 0:
+			print "reading from camera failed"
+		gray_scale = cv2.cvtColor(self.frame,cv2.COLOR_BGR2GRAY)
+		imBw = gray_scale	> 100
+		self.axes.clear()
+		if self.kShowType == 0:
+			hIm = self.axes.imshow(self.frame[:,:,[2,1,0]])
+		#~ hIm.set_array(self.frame[:,:,[2,1,0]])
+		elif self.kShowType == 1:
+			
+			hIm = self.axes.imshow(gray_scale)
+		else:
+			#~ gray_scale = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+			
+			hIm = self.axes.imshow(imBw)
+		
+		#~ plt.show()
+		canvas1.show()
+		toc = time.time()
+		#~ print toc - tic
+		data = {'1': {'Time': '{0:.3f}'.format(toc - tic),'Gray_value': '{}'.format(gray_scale.min())}}
+		model = table.model
+		model.importDict(data)
+		table.redrawTable()
+		return hIm
+	def changeFrame(self, event):
+		if self.kShowType <= 1:
+			self.kShowType = self.kShowType + 1
+		else:
+			self.kShowType = 0
+			#~ print self.kShowType, "kShowType"
+		print "ShowFrameType k upgraded to: ", self.kShowType
 #Function for starting timers
 def video_start():
     timerFrameDisplay.start()
     #~ t.start()
-
+    
 #Function for stopping timers
 def video_stop():
     timerFrameDisplay.stop()
     #~ t.stop()
 
+objShowFrame = ShowFrame()
+objShowFrame.captureDevice = cap
+objShowFrame.axes = axis1
+objShowFrame.hIm = hIm
+
 #Timer for frame
-timerFrameDisplay = f2.canvas.new_timer(interval=10)
-timerFrameDisplay.add_callback(showFrame, hIm)
+timerFrameDisplay = f2.canvas.new_timer(interval=1)
+timerFrameDisplay.add_callback(objShowFrame.showFrame)
 
 #Timer for video
 #~ t = RepeatedTimer(0.01, video)
@@ -140,4 +182,13 @@ timerFrameDisplay.add_callback(showFrame, hIm)
 b1 = Button(master, text="Start", bg='white', command=video_start).place(x=50, y=600)
 b2 = Button(master, text="Stop", bg='white', command=video_stop).place(x=400, y=600)
 #~ master.mainloop()
+
+
+
+#~ kShowFrameType = KShowFrameType(0)
+#~ KShowFrameType(0)
+
+#~ canvas1.mpl_connect('button_press_event', lambda e: ShowFrame().callback(e,kShowFrameType))
+canvas1.mpl_connect('button_press_event', objShowFrame.changeFrame)
+#~ kShowFrameType = 0
 
